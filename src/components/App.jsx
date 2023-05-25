96d25bed5a5a15d68b58464dddc98aecccacc021
Ch1mik <125959505+Ch1mik@users.noreply.github.com>
2023-05-26 01:13:29 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
-export const App = () => {
-  return (
-    <div
+import React, { Component } from "react";
+import s from './App.module.css';
+import Feedback from "./Feedback/Feedback";
+import Section from "./Section/Section";
+import Statistics from "./Statistics/Statistics";
+import Notification from "./Notification/Notification";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+  onLeaveFeedback = state => {
+    this.setState(prevState => ({ [state]: prevState[state] + 1 }))
+  };
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+ countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+  const totalFeedback = this.countTotalFeedback();
+  return totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+  }
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+
+    return (
+      <div
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        backgroundColor: '#93c5fa'
       }}
     >
-      React homework template
-    </div>
-  );
+      <div className={s.container}>
+        <Section title="Please leave feedback">
+          <Feedback
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+          </Section>
+          <Section title="Statistics">
+            {this.countTotalFeedback() > 0 ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={this.countTotalFeedback()}
+                positivePercentage={this.countPositiveFeedbackPercentage()}
+              />
+            ) : (
+               <Notification message="There is no feedback"></Notification> 
+            )}
+          </Section>
+        </div>
+      </div>
+    )
+  }
 };
+
+
+// return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       <Feedback/>
+//     </div>
+//   );
